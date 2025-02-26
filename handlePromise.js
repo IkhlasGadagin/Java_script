a9d289eac9687821f14a438b4c73801e2c8c58f0
IkhlasGadagin <ikhlashgadagin@gmail.com>
2025-02-26 18:01:28 +0530

    multiple function is handles as promise

--- handlePromise.js
+++ handlePromise.js
@@ -3,7 +3,7 @@ function getDataFromDataBase() {
     try {
         return new Promise((res, rej) => {
             return setTimeout(() => {
-                rej("Failure")
+                res("The Data Got successfully")
             }, 5000)
         })
     } catch (error) {
@@ -11,11 +11,28 @@ function getDataFromDataBase() {
     }
 }
 
+function daPayment() {
+
+    try {
+        return new Promise((res, rej) => {
+            setTimeout(() => {
+                return res("the payment done successfully")
+            }, 5000)
+        })
+    } catch (error) {
+        console.log(error);
+
+    }
+}
 
 
 getDataFromDataBase()
     .then((data) => {
         console.log("when the data is Resolved", data)
+    })
+    .then(daPayment)
+    .then((data) => {
+        console.log("the payment is donr", data)
     })
     .catch((error) => {
         console.log("when the dta is Rejected", error)
