742ccc90eba0e73c89063fd2dabbbbef7337c45c
IkhlasGadagin <ikhlashgadagin@gmail.com>
2025-02-26 14:17:18 +0530

    just remember the syntax of the loops

---
+++ forloop.js
@@ -0,0 +1,35 @@
+// the main use of the loop is to iterate the string or the array or unto the condition is khatam 
+//inner loop is also opossible types of loop are 
+// for , for in , for of 
+
+
+// function abiwakas(){
+//     for(let i=0; i<=10; i++){
+//         console.log(`the value out side j ${i}`)
+//         for(let j=0; j<=10; j++){
+//             console.log(`the  value inside j loop ${j}J  ${i}I`)
+
+//         }
+
+//     }
+
+// }
+// abiwakas()
+
+// const myObject = {
+//     name: "ikhlas",
+//     adress: "gadagin"
+// }
+
+// for (let value in myObject) {
+//     console.log(value)
+// }
+// console.log("for loop")
+
+const arr = [2, 3, 4, 2, 4, 2, 2]
+
+const ans = arr.forEach((element) => {
+    console.log(element * 2);
+
+})
+console.log(ans);
